c8bbcd55e19534042700347ddfae90e7fcbf6cea
thaismota-tech <thaissantosdamota@gmail.com>
2026-05-04 16:39:17 +0200

    He finalizado el ejercicio 2

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 let [a = 5, b = 10] = [10, 5];
 console.log(a);
 console.log(b);
+
+//EJERCICIO 2  Obtener el segundo elemento de un array
+function segundoElemento(array) {
+  return array[1]; //se pone 1 porque el segundo número del array está en esa orden según el índice
+}
+console.log(segundoElemento([1, 2, 3, 4]));
